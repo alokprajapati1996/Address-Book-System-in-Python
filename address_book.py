5b10dfecb722e0319b16ccc0273a26d8aebf5541
unknown <alokprajapati913@gmail.com>
2023-12-13 12:43:28 +0530

    [Alok] Add:script to create add method on access all information of person

--- address_book.py
+++ address_book.py
@@ -25,6 +25,23 @@ class Contact:
         self.email_id = email_id
 
 
+class AddressBookMain:
+
+    def __init__(self, book_name):
+        self.cont_person_dict = {}
+        self.book_name = book_name
+
+    def add_contact(self, contact_obj):
+        """
+        Description : This method made for add information of person.
+        parameter : None
+        return : None
+
+        """
+
+        self.cont_person_dict.update({contact_obj.first_name: contact_obj})
+
+
 def main():
     """
     Description : This function made for access information of contact.
@@ -32,15 +49,28 @@ def main():
     return : None
 
     """
-    first_name = input("Enter first name: ")
-    last_name = input("Enter first name: ")
-    address = input("Enter first name: ")
-    city = input("Enter first name: ")
-    state = input("Enter first name: ")
-    zip_code = input("Enter first name: ")
-    phone_no = input("Enter first name: ")
-    email_id = input("Enter first name: ")
-    contact = Contact(first_name, last_name, address, city, state, zip_code, phone_no, email_id)
+    print("Welcome to Address Book Program in AddressBookMain class on START Master Branch")
+    book_name = input("Enter book name: ")
+    add_book_obj = AddressBookMain(book_name)
+
+    print("0.exit : \n 1.Add person details: ")
+
+    while True:
+        your_choice = int(input("Enter your choice: "))
+        match your_choice:
+            case 1:
+                first_name = input("Enter first name: ")
+                last_name = input("Enter last name: ")
+                address = input("Enter address : ")
+                city = input("Enter city name: ")
+                state = input("Enter state name: ")
+                zip_code = int(input("Enter zip code  name: "))
+                phone_no = input("Enter mobile no.: ")
+                email_id = input("Enter email id: ")
+                contact_obj = Contact(first_name, last_name, address, city, state, zip_code, phone_no, email_id)
+                add_book_obj.add_contact(contact_obj)
+            case 0:
+                break
 
 
 if __name__ == '__main__':
